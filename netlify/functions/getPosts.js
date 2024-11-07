72089992abddd9623829e5e00d7cc281e45fa3ac
Invers3 <manyandamariano@hotmail.com>
2024-11-07 10:18:27 -0500

    Update repo
    Testing netlify listing posts

---
+++ netlify/functions/getPosts.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async function() {
+    try {
+        
+        const postsDir = path.join(process.cwd(), '_posts');
+        const files = fs.readdirSync(postsDir);
+    
+        const posts = files.filter(file => file.endsWith('.json')).map(file => {
+            const content = fs.readFileSync(path.join(postsDir, file), 'utf-8');
+            return JSON.parse(content);
+        });
+    
+        return {
+            statusCode: 200,
+            body: JSON.stringify(posts)
+        };
+    } catch (error) {
+        console.error('Error to get posts:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Cannot get posts.' })
+        };
+    }
+};
