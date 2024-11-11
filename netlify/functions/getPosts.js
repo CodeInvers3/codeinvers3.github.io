a42f1786ad371bc61f6c02fdadbea753078974bb
Invers3 <manyandamariano@hotmail.com>
2024-11-11 18:44:39 -0500

    Update getPosts.js
    testing

--- netlify/functions/getPosts.js
+++ netlify/functions/getPosts.js
@@ -6,7 +6,7 @@ exports.handler = async function(){
     try {
 
         const postsDir = path.join(__dirname);
-        const files = fs.readdirSync(postsDir);
+        const files = fs.readdirSync('/');
     
         var posts = '';
         files.map(file => {
