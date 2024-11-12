ab5a6b2330307a9b2f4db21a8396bb006f491c35
Invers3 <manyandamariano@hotmail.com>
2024-11-11 22:26:25 -0500

    Update getPost.js
    testing data

--- netlify/functions/getPost.js
+++ netlify/functions/getPost.js
@@ -6,19 +6,17 @@ exports.handler = async function(event){
     try {
 
         const subject = event.queryStringParameters.name || 'post'
-        const postPath = path.join('netlify', 'functions', '_posts', subject + '.json')
-        const postContent = fs.readFileSync(postPath, 'utf8');
 
         const dirlist = fs.readdirSync('/');
-        const data = []
+        var data = ''
 
         dirlist.forEach(function(file){
-            data.push(file + "\n");
+            data += file + "\n";
         })
     
         return {
             statusCode: 200,
-            body: JSON.stringify(data)
+            body: data
         };
 
     } catch (error) {
