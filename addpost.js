cb5e79a6b9a353c507f6089f86f971e87720107b
Invers3 <manyandamariano@hotmail.com>
2024-11-18 00:50:28 -0500

    Update addpost.js
    Testing

--- addpost.js
+++ addpost.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
+const path = require('path');
 
 const newPost = {
   title: 'Mi primer post',
   content: 'Este es el contenido de mi primer post'
 };
 
-fs.writeFile('post.json', JSON.stringify(newPost), (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Archivo post.json creado exitosamente');
-  }
-});
+fs.writeFileSync(path.join(__dirname, 'hello_post.json'), JSON.stringify(newPost), 'utf8');
+console.log('Archivo post.json creado exitosamente');
